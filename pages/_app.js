90510a540d5e9dc97e18a4eaacd7283b3bc395a4
Bruno Stersa <brunostersa@MacBook-Air-de-Bruno.local>
2025-08-25 00:58:20 -0300

    feat: adiciona verificação do Google Search Console no header global

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,13 @@
 import '../styles/globals.css'
+import Head from 'next/head'
 
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <meta name="google-site-verification" content="ip0QC2xuodmc80u5Y291yiARaLWbF_-b9lWVHlrowFw" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
